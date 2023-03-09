9eab6dd89d414aa5f7457c894703138467eccc52
Юлия <Yulechka140200@gmail.com>
2023-03-09 15:48:28 +0500

    дз методы объектов

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         Author pushkin = new Author("Александр", "Пушкин");
+        Author pushkin2 = new Author("Александр", "Пушкин");
         Author tolstoy = new Author("Лев", "Толстой");
 
         Book eugeneOnegin = new Book("Евгений Онегин", 1831, pushkin);
+        Book eugeneOnegin2 = new Book("Евгений Онегин", 1831, pushkin);
         Book warAndPeace = new Book("Война и мир", 1854, tolstoy);
 
-        eugeneOnegin.printInfo();
-        warAndPeace.printInfo();
+        System.out.println(eugeneOnegin.equals(eugeneOnegin2));
+        System.out.println(warAndPeace);
 
-        eugeneOnegin.setPublishingYear(1823);
-        eugeneOnegin.printInfo();
     }
 
 }
